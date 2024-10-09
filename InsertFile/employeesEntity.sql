bd322f078f112e45b572dd41e7946ce2d733f3b9
MewKina <141310839+MewKina@users.noreply.github.com>
2024-10-09 19:51:40 +0700

    Update employeesEntity.sql

--- InsertFile/employeesEntity.sql
+++ InsertFile/employeesEntity.sql
@@ -1,13 +1,14 @@
-INSERT INTO Employees (first_name, last_name, date_of_birth, hire_date, position, type) VALUES
-('John', 'Doe', '1980-03-14', '2010-01-05', 'Manager', 'full-time'),
-('Emily', 'Smith', '1992-06-23', '2018-05-12', 'Waiter', 'part-time'),
-('Michael', 'Brown', '1985-08-19', '2012-07-22', 'Chef', 'full-time'),
-('Jessica', 'Johnson', '1995-10-25', '2020-09-15', 'Host', 'part-time'),
-('David', 'Miller', '1988-12-31', '2015-11-09', 'Waiter', 'full-time'),
-('Sarah', 'Davis', '1991-07-07', '2017-04-05', 'Host', 'part-time'),
-('James', 'Wilson', '1983-01-11', '2009-02-16', 'janitor', 'full-time'),
-('Laura', 'Taylor', '1990-09-21', '2016-06-19', 'Chef', 'full-time'),
-('Daniel', 'Moore', '1984-04-17', '2011-03-11', 'Waiter', 'full-time'),
-('Sophia', 'Martinez', '1993-05-30', '2019-08-25', 'janitor', 'part-time'),
-('Oliver', 'Anderson', '1987-11-08', '2014-01-28', 'Waiter', 'full-time'),
-('Emma', 'Clark', '1994-02-22', '2021-07-14', 'Waiter', 'part-time');
+-- employees inserts 
+INSERT INTO Employees (emp_id, first_name, last_name, date_of_birth, hire_date, position, type) VALUES
+(1, 'John', 'Doe', '1980-03-14', '2010-01-05', 'Manager', 'full-time'),
+(2, 'Emily', 'Smith', '1992-06-23', '2018-05-12', 'Waiter', 'part-time'),
+(3, 'Michael', 'Brown', '1985-08-19', '2012-07-22', 'Chef', 'full-time'),
+(4, 'Jessica', 'Johnson', '1995-10-25', '2020-09-15', 'Host', 'part-time'),
+(5, 'David', 'Miller', '1988-12-31', '2015-11-09', 'Waiter', 'full-time'),
+(6, 'Sarah', 'Davis', '1991-07-07', '2017-04-05', 'Host', 'part-time'),
+(7, 'James', 'Wilson', '1983-01-11', '2009-02-16', 'Janitor', 'full-time'),
+(8, 'Laura', 'Taylor', '1990-09-21', '2016-06-19', 'Chef', 'full-time'),
+(9, 'Daniel', 'Moore', '1984-04-17', '2011-03-11', 'Waiter', 'full-time'),
+(10, 'Sophia', 'Martinez', '1993-05-30', '2019-08-25', 'Janitor', 'part-time'),
+(11, 'Oliver', 'Anderson', '1987-11-08', '2014-01-28', 'Waiter', 'full-time'),
+(12, 'Emma', 'Clark', '1994-02-22', '2021-07-14', 'Waiter', 'part-time');
